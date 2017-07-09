9d4a6731071055d4e04b333557357d4376ff3836
Q-Mart <qumarthjash@gmail.com>
2017-07-09 10:48:46 +0100

    Initial work

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,44 @@ module Main where
 
 import Lib
 
+data BrainFuckCommand = GoLeft
+                      | GoRight
+                      | Increment
+                      | Decrement
+                      | Print
+                      | Read
+                      | LoopL
+                      | LoopR
+                      | Comment Char
+
+data BrainFuckSource = BrainFuckSource [BrainFuckCommand]
+instance Show BrainFuckSource where
+  show (BrainFuckSource commands) = map bfToChar commands
+    where
+      bfToChar x = case x of
+        GoRight   -> '>'
+        GoLeft    -> '<'
+        Increment -> '+'
+        Decrement -> '-'
+        Print     -> '.'
+        Read      -> ','
+        LoopL     -> '['
+        LoopR     -> ']'
+        Comment c -> c
+
+parseBrainFuck :: String -> BrainFuckSource
+parseBrainFuck = BrainFuckSource . map charToBF
+  where
+    charToBF x = case x of
+      '>' -> GoRight
+      '<' -> GoLeft
+      '+' -> Increment
+      '-' -> Decrement
+      '.' -> Print
+      ',' -> Read
+      '[' -> LoopL
+      ']' -> LoopR
+      c   -> Comment c
+
 main :: IO ()
-main = someFunc
+main = putStrLn "Hello world"
